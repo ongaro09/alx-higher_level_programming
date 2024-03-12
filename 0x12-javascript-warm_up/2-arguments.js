7a7043cc135f55bad8f38add3a972359b8ebe4fd
ongaro09 <ongaromark7@gmail.com>
2024-03-12 23:40:59 +0300

    how to use destructuring assignment

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 
-const { argv } = require('node:process');
+const args = process.argv.slice(2);
 
-if (argv.length === 2) {
-  console.log('No arguments');
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
 } else {
-  console.log('Argument found');
+  console.log("Arguments found");
 }
