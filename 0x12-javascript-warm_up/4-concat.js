246a7baf454c01d784d7a45d467e00747e6b7766
ongaro09 <ongaromark7@gmail.com>
2024-03-13 19:09:48 +0300

    concat operation

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const [, , arg1, arg2] = process.argv;
+
+if (arg1 && arg2) {
+  console.log(`${arg1} is ${arg2}`);
+} else if (arg1) {
+  console.log(`${arg1} is undefined`);
+} else {
+  console.log('undefined is undefined');
+}
